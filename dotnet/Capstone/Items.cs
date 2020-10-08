76d52b04829aa49197cf509d332df13e5358a695
Zachary Workman <Zacharyyy.1994@gmail.com>
2020-10-08 12:23:41 -0400

    Hello

--- dotnet/Capstone/Items.cs
+++ dotnet/Capstone/Items.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.IO;
 namespace Capstone
 {
     class Items
     {
+        public static void DisplayItems()
+        {
+            Console.Clear();
+            Console.Write("\n");
+            //ask user for filePath
+            string directory = @"C:\Users\Zachary\Workspace\module1-capstone-c-team-6\Example Files\Inventory.txt";
+            string directoryJuan = @"C:\Users\Student\Workspace\module1-capstone-c-team-6\Example Files\Inventory.txt";
+            using (StreamReader sw = new StreamReader(directory))
+            {
+                while (!sw.EndOfStream)
+                {
+                    //Make sure item is avaliable otherwise display SOLD OUT
+                    string lineContents = sw.ReadLine();
+                    Console.WriteLine(lineContents);
+                    /*
+                    int lineNum = 1;
+                    //ADDING EACH LINE TO ITS OWN ARRAY
+                    string hello = new string(lineContents);
+                    ItemList.Add(hello);
+                    string[] splitLine = hello.Split("|");
+                    //ITEM CHARACTERISTICS
+                    string buttonNumber = splitLine[0];
+                    string name = splitLine[1];
+                    decimal price = decimal.Parse(splitLine[2]);
+                    string type = splitLine[3];
+                    lineNum++;
+                    */
+                }
+            }
+        }
     }
 }
--- dotnet/Capstone/Program.cs
+++ dotnet/Capstone/Program.cs
@@ -1,39 +1,41 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
-
 namespace Capstone
 {
     class Program
     {
         private const string MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-    	private const string MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	    private readonly string[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE }; //const has to be known at compile time, the array initializer is not const in C#
-
+        private const string MAIN_MENU_OPTION_PURCHASE = "Purchase";
+        private const string MAIN_MENU_OPTION_FINISHED = "Finished";
+        private readonly string[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_FINISHED }; //const has to be known at compile time, the array initializer is not const in C#
         private readonly IBasicUserInterface ui = new MenuDrivenCLI();
-
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Run();
         }
-
         public void Run()
         {
-            while(true) //rn this is an infinite loop. You'll need a 'finished' option and then you'll break after that option is selected
+            while (true)
             {
                 String selection = (string)ui.PromptForSelection(MAIN_MENU_OPTIONS);
-                if (selection==MAIN_MENU_OPTION_DISPLAY_ITEMS)
+                if (selection == MAIN_MENU_OPTION_DISPLAY_ITEMS)
                 {
-                    //display the vending machine items (probably should call a method to do this)
+                    Items.DisplayItems();
                 }
-                else if (selection==MAIN_MENU_OPTION_PURCHASE)
+                else if (selection == MAIN_MENU_OPTION_PURCHASE)
                 {
                     //do the purchase (probably should call a method to do this too)
+                    //if (balance == 0)
+                    //{
+                    //}
+                    //else { }
                 }
-
+                else if (selection == MAIN_MENU_OPTION_FINISHED)
+                {
+                    break;
+                }
             }
         }
-
-
     }
 }
