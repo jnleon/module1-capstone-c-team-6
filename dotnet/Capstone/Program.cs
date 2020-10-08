70581b98334c85d0432c402f506b6b62ffe489d0
Katie Dwyer <katie.dwyer@techelevator.com>
2020-10-08 06:24:37 -0400

    capstone

---
+++ dotnet/Capstone/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Capstone
+{
+    class Program
+    {
+        private const string MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+    	private const string MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	    private readonly string[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE }; //const has to be known at compile time, the array initializer is not const in C#
+
+        private readonly IBasicUserInterface ui = new MenuDrivenCLI();
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+        public void Run()
+        {
+            while(true) //rn this is an infinite loop. You'll need a 'finished' option and then you'll break after that option is selected
+            {
+                String selection = (string)ui.PromptForSelection(MAIN_MENU_OPTIONS);
+                if (selection==MAIN_MENU_OPTION_DISPLAY_ITEMS)
+                {
+                    //display the vending machine items (probably should call a method to do this)
+                }
+                else if (selection==MAIN_MENU_OPTION_PURCHASE)
+                {
+                    //do the purchase (probably should call a method to do this too)
+                }
+
+            }
+        }
+
+
+    }
+}
